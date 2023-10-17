a51007ab927c08a313f916ce0a1dada7c565b008
oopenko <olena.openko@jambit.com>
2023-10-17 20:47:10 +0200

    Added a skeleton for the ToDo list

---
+++ src/main/java/pp/olena/todo/controller/TodoListController.java
@@ -0,0 +1,12 @@
+package pp.olena.todo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class TodoListController {
+
+}
