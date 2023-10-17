11064afabd20fb9b5bb49525dce4780fec87bb7d
oopenko <olena.openko@jambit.com>
2023-10-17 21:17:05 +0200

    Added comments to track what must be done and where.

--- src/main/java/pp/olena/todo/controller/TodoListController.java
+++ src/main/java/pp/olena/todo/controller/TodoListController.java
@@ -16,32 +16,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import pp.olena.todo.dto.Status;
 import pp.olena.todo.dto.Task;
-
-/**
- * todo update the controller to follow the requirements
- *
- * add an item,
- * change description of an item,
- * mark an item as "done",
- * mark an item as "not done",
- * get all items that are "not done" (with an option to retrieve all items),
- * get details of a specific item.
- * The service should automatically change status of items that are past their due date as "past due".
- * The service should forbid changing "past due" items via its REST API.
- */
 
 @RestController
 @RequestMapping("/api")
 @Slf4j
 public class TodoListController {
 
+    /**
+     * Get details of a specific item.
+     * @param id Long id of the Task.
+     */
     @GetMapping(value = "/tasks/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getTask(@PathVariable @NotBlank Long id) {
         //get a task - todo implementation
         return ResponseEntity.status(HttpStatus.OK).body("Future task");
     }
 
+    /**
+     * Create a task.
+     * @param task {@link Task}
+     */
     @PostMapping(value = "/tasks", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<?> createTask(@RequestBody @Valid @NotNull Task task) {
@@ -49,6 +45,10 @@ public class TodoListController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    /**
+     * Allows to change description of an item, mark an item as "done", mark an item as "not done".
+     * @param task {@link Task}
+     */
     @PutMapping(value = "/tasks", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<?> updateTask(@RequestBody @Valid @NotNull Task task) {
@@ -58,13 +58,19 @@ public class TodoListController {
 
     @DeleteMapping(value = "/tasks", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> deleteTask(@PathVariable @NotBlank Long id) {
-        //get a task - todo implementation
+        //delete a task - todo implementation
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @GetMapping(value = "/tasks", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getAllTasks() {
-        //get a task
+        //get all tasks - todo implementation
+        return ResponseEntity.status(HttpStatus.OK).body("Future list of tasks");
+    }
+
+    @GetMapping(value = "/tasks/status/{status}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getAllTasks(@PathVariable @NotBlank Status status) {
+        //get all tasks - todo implementation - consider using some kind of filter
         return ResponseEntity.status(HttpStatus.OK).body("Future list of tasks");
     }
 }
