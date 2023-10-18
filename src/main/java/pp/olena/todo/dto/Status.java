202b3eb8bc759603636ab8df43c214ecd5137cfa
oopenko <olena.openko@jambit.com>
2023-10-18 18:35:00 +0200

    Added status of the task.

--- src/main/java/pp/olena/todo/dto/Status.java
+++ src/main/java/pp/olena/todo/dto/Status.java
@@ -1,5 +1,25 @@
 package pp.olena.todo.dto;
 
-//todo status options status: "not done", "done", "past due"
+import lombok.Getter;
+
+/**
+ * Status of the task: not done, done, past due.
+ */
 public enum Status {
+    NOT_DONE("not done"), DONE("done"), PAST_DUE("past due");
+    @Getter
+    private final String value;
+
+    Status(String value) {
+        this.value = value;
+    }
+
+    public static Status fromString(String value) {
+        for (Status status : Status.values()) {
+            if (status.value != null && status.value.equals(value)) {
+                return status;
+            }
+        }
+        throw new RuntimeException("Status with the value " + value + " does not exist.");
+    }
 }
