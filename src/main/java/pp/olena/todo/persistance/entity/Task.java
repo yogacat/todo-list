2513f9619abb1f69d3f0b296ca372319b6b20d0d
oopenko <olena.openko@jambit.com>
2023-10-18 18:41:38 +0200

    Added task entity.

---
+++ src/main/java/pp/olena/todo/persistance/entity/Task.java
@@ -0,0 +1,43 @@
+package pp.olena.todo.persistance.entity;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.data.annotation.CreatedDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "task")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String description;
+
+    @NonNull
+    @Pattern(regexp = "^(not done|done|past due)$")
+    private String status;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Nullable
+    private LocalDateTime dueTo;
+
+    @Nullable
+    private LocalDateTime doneAt;
+}
