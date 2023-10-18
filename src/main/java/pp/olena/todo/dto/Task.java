f8a3386f3a5762ee6b9ffd7162f69d13c2b6ff62
oopenko <olena.openko@jambit.com>
2023-10-18 18:51:36 +0200

    Added a record for the task

--- src/main/java/pp/olena/todo/dto/Task.java
+++ src/main/java/pp/olena/todo/dto/Task.java
@@ -3,7 +3,16 @@ package pp.olena.todo.dto;
 import org.springframework.data.annotation.CreatedDate;
 import java.time.LocalDateTime;
 
-//todo validation
-public record Task(String description, Status status, @CreatedDate LocalDateTime createdAt, LocalDateTime dueTo, LocalDateTime doneAt) {
+/**
+ * Data object that I will return in the controller to the requestor.
+ * @param id Long id of the task
+ * @param description String
+ * @param status {@link Status}
+ * @param createdAt LocalDateTime
+ * @param dueTo LocalDateTime
+ * @param doneAt LocalDateTime
+ */
+public record Task(Long id, String description, Status status, @CreatedDate LocalDateTime createdAt,
+                   LocalDateTime dueTo, LocalDateTime doneAt) {
 
 }
