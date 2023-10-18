8242bd82ce95a1c6412a1068298dc58b9a6a639b
oopenko <olena.openko@jambit.com>
2023-10-18 18:27:28 +0200

    Added javadocs and validation. Added implementation via Service.

--- src/main/java/pp/olena/todo/controller/TodoListController.java
+++ src/main/java/pp/olena/todo/controller/TodoListController.java
@@ -1,9 +1,9 @@
 package pp.olena.todo.controller;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,61 +16,84 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import pp.olena.todo.dto.Status;
 import pp.olena.todo.dto.Task;
+import pp.olena.todo.dto.TaskFilter;
+import pp.olena.todo.dto.TaskId;
+import pp.olena.todo.service.TaskService;
+import pp.olena.todo.validation.CreateTask;
+import pp.olena.todo.validation.UpdateTask;
+import java.util.List;
 
+/**
+ * Controller for the API, allows to create/update/edit/delete and retrieve items.
+ */
 @RestController
 @RequestMapping("/api")
 @Slf4j
 public class TodoListController {
 
+    private final TaskService taskService;
+
+    @Autowired
+    public TodoListController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     /**
      * Get details of a specific item.
      * @param id Long id of the Task.
+     * @return ResponseEntity
      */
     @GetMapping(value = "/tasks/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getTask(@PathVariable @NotBlank Long id) {
-        //get a task - todo implementation
-        return ResponseEntity.status(HttpStatus.OK).body("Future task");
+    public ResponseEntity<Task> getTask(@PathVariable @NotBlank Long id) {
+        Task task = taskService.getTaskById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 
     /**
      * Create a task.
      * @param task {@link Task}
+     * @return ResponseEntity
      */
     @PostMapping(value = "/tasks", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity<?> createTask(@RequestBody @Valid @NotNull Task task) {
-        //create a task - todo implementation
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<TaskId> createTask(@RequestBody @CreateTask @NotNull Task task) {
+        TaskId taskId = taskService.createTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskId);
     }
 
     /**
-     * Allows to change description of an item, mark an item as "done", mark an item as "not done".
+     * Updates a task. Allows to change description of an item, mark an item as "done", mark an item as "not done". Will
+     * not change an item that is "past due".
      * @param task {@link Task}
+     * @return ResponseEntity
      */
     @PutMapping(value = "/tasks", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity<?> updateTask(@RequestBody @Valid @NotNull Task task) {
-        //update a task - todo implementation
+    public ResponseEntity<?> updateTask(@RequestBody @UpdateTask @NotNull Task task) {
+        taskService.updateTask(task);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    /**
+     * Deletes a task by id.
+     * @param id Long id of the task to delete
+     * @return ResponseEntity
+     */
     @DeleteMapping(value = "/tasks", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> deleteTask(@PathVariable @NotBlank Long id) {
-        //delete a task - todo implementation
+        taskService.deleteTask(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @GetMapping(value = "/tasks", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAllTasks() {
-        //get all tasks - todo implementation
-        return ResponseEntity.status(HttpStatus.OK).body("Future list of tasks");
-    }
-
-    @GetMapping(value = "/tasks/status/{status}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAllTasks(@PathVariable @NotBlank Status status) {
-        //get all tasks - todo implementation - consider using some kind of filter
-        return ResponseEntity.status(HttpStatus.OK).body("Future list of tasks");
+    /**
+     * Get all tasks by filter parameters.
+     * @param filter {@link TaskFilter}
+     * @return ResponseEntity
+     */
+    @GetMapping(value = "/tasks/filter/{filter}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Task>> getAllTasks(@PathVariable @NotNull TaskFilter filter) {
+        List<Task> tasks = taskService.getAllByFilter(filter);
+        return ResponseEntity.status(HttpStatus.OK).body(tasks);
     }
 }
