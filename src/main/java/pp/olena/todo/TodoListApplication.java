53d368d2abd3507f915a02609cb8e8446515b4fd
oopenko <olena.openko@jambit.com>
2023-10-19 23:23:14 +0200

    Added todo not to forget a requirement

--- src/main/java/pp/olena/todo/TodoListApplication.java
+++ src/main/java/pp/olena/todo/TodoListApplication.java
@@ -3,6 +3,7 @@ package pp.olena.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//todo add a scheduler to change status of the task
 @SpringBootApplication
 public class TodoListApplication {
 
