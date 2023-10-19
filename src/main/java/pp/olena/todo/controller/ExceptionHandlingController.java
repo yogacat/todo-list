b2fadf408d2de6583d7ff9ecaeabf51638651f87
oopenko <olena.openko@jambit.com>
2023-10-19 22:49:10 +0200

    Added a skeleton for the error mapper

---
+++ src/main/java/pp/olena/todo/controller/ExceptionHandlingController.java
@@ -0,0 +1,11 @@
+package pp.olena.todo.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * Converts exceptions from the application into response entity.
+ */
+@ControllerAdvice
+public class ExceptionHandlingController {
+    //todo mapping for the exceptions
+}
