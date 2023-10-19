10f7019e4359ca45be9fde7d9248820b96a9166f
oopenko <olena.openko@jambit.com>
2023-10-19 22:47:02 +0200

    Removed double validation

--- src/main/java/pp/olena/todo/dto/CreateTask.java
+++ src/main/java/pp/olena/todo/dto/CreateTask.java
@@ -1,13 +1,10 @@
 package pp.olena.todo.dto;
 
-import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.NotBlank;
-import pp.olena.todo.validation.FutureDateTime;
 import java.time.LocalDateTime;
 
 /**
  * Data object that contains mandatory parameters for the validation when the task is being created.
  */
-public record CreateTask(@NotBlank String description, @Nullable @FutureDateTime LocalDateTime dueTo) {
+public record CreateTask(String description, LocalDateTime dueTo) {
 
 }
