56eccca11913607030b427e97e07e15cfac29eed
oopenko <olena.openko@jambit.com>
2023-10-23 18:43:54 +0200

    Added a method to collect all past due that aren't set to past due yet.

--- src/main/java/pp/olena/todo/persistance/repository/TaskRepository.java
+++ src/main/java/pp/olena/todo/persistance/repository/TaskRepository.java
@@ -2,9 +2,13 @@ package pp.olena.todo.persistance.repository;
 
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pp.olena.todo.persistance.entity.Task;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAll(Specification<Task> spec);
+
+    @Query("SELECT t FROM Task t WHERE t.status <> 'done' AND t.dueTo < CURRENT_TIMESTAMP")
+    List<Task> findAllWithDueToBeforeNowAndStatusNotDone();
 }
