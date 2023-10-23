e52fcc757c149d8e3fbc4addafa443460c5dec56
oopenko <olena.openko@jambit.com>
2023-10-23 18:17:27 +0200

    Added exceptions mapping to send an error back as a JSON in case if something didn't happen (and update for example).

--- src/main/java/pp/olena/todo/controller/ExceptionHandlingController.java
+++ src/main/java/pp/olena/todo/controller/ExceptionHandlingController.java
@@ -1,11 +1,38 @@
 package pp.olena.todo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import pp.olena.todo.dto.ErrorMessage;
+import pp.olena.todo.exception.NoChangesException;
+import pp.olena.todo.exception.StatusNotFoundException;
+import pp.olena.todo.exception.TaskNotFoundException;
+import pp.olena.todo.exception.TaskUpdateForbiddenException;
 
 /**
  * Converts exceptions from the application into response entity.
  */
 @ControllerAdvice
 public class ExceptionHandlingController {
-    //todo mapping for the exceptions
+
+    @ExceptionHandler(StatusNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handleWrongStatus(StatusNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage(ex.getMessage()));
+    }
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handleWrongTaskId(TaskNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage(ex.getMessage()));
+    }
+
+    @ExceptionHandler(TaskUpdateForbiddenException.class)
+    public ResponseEntity<ErrorMessage> handleForbiddenUpdate(TaskUpdateForbiddenException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorMessage(ex.getMessage()));
+    }
+
+    @ExceptionHandler(NoChangesException.class)
+    public ResponseEntity<ErrorMessage> handleNoChanges(NoChangesException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_MODIFIED).body(new ErrorMessage(ex.getMessage()));
+    }
 }
---
+++ src/main/java/pp/olena/todo/dto/ErrorMessage.java
@@ -0,0 +1,8 @@
+package pp.olena.todo.dto;
+
+/**
+ * A structure that contains String message that will be turned into a JSON to be sent to the caller.
+ */
+public record ErrorMessage(String message) {
+
+}
